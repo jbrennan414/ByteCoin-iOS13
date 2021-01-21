5da96c4ba7bbfc4d30978072bb6395367fe7be16
John Brennan <jbrennan@fluentstream.com>
2021-01-20 22:07:32 -0700

    lots of changes

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by John Brennan on 1/20/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
